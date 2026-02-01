625a04924acdf20714a1f1eb117426c7f3bfa004
Grace <liug06@mit.edu>
2026-01-31 23:05:13 -0500

    trying to refactor ped_direction

--- ped_direction.py
+++ ped_direction.py
@@ -1,5 +1,16 @@
 import jax
 import jax.numpy as jnp
 
-class Pedestrian:
-    lo
+def semi_implicit_euler(x: jnp.ndarray, speed:jnp.ndarray, theta:jnp.ndarray, a:jnp.ndarray, alpha: jnp.ndarray, dt):
+    speed_next = speed + a * dt 
+    theta_next = theta + alpha * dt
+    directions = jnp.stack([jnp.cos(theta), jnp.sin(theta)], axis=-1)
+    x_next = x + dt * speed[:,None] * directions
+    return x_next, speed_next, theta_next
+
+def step_destination(carry, rng):
+    x_next, speed_next, theta_next = carry
+    return (x_next, speed_next, theta_next), (x_next, speed_next, theta_next)
+
+def generate_data():
+    pass 
